be297e70252b8d4e37ffc1649cdf1e7006b90d31
Alex Blair <bclan11@gmail.com>
2022-05-09 12:44:24 -0700

    routes all made

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,38 @@
+import { Link, Route, Switch } from 'react-router-dom';
+import Characters from './views/Characters';
+import Episodes from './views/Episodes';
+import Locations from './views/Locations';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <header>
+        <h1>Welcome to the Rick and Morty API explorer</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/characters">character list</Link>
+            </li>
+            <li>
+              <Link to="/locations">locations</Link>
+            </li>
+            <li>
+              <Link to="/episodes">episodes</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <Switch>
+        <Route path="/characters">
+          <Characters />
+        </Route>
+        <Route path="/locations">
+          <Locations />
+        </Route>
+        <Route path="/episodes">
+          <Episodes />
+        </Route>
+      </Switch>
+    </>
+  );
 }
---
+++ src/views/Characters.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Characters() {
+  return (
+    <>
+      <div>Characters</div>
+    </>
+  );
+}
